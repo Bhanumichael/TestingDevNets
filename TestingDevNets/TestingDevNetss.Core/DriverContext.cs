6d88bf9a9314a065f5e9b5630a4d552b528c8006
waghmayur418 <waghmayur418@gmail.com>
2024-05-01 13:01:03 +0530

    Updated DriverContext class and created new UiHelper class to Core project

--- TestingDevNets/TestingDevNetss.Core/DriverContext.cs
+++ TestingDevNets/TestingDevNetss.Core/DriverContext.cs
@@ -1,12 +1,23 @@
-﻿namespace TestingDevNetss.Core
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+namespace TestingDevNetss.Core
 {
     public class DriverContext
     {
-        public void StartDriver(string browser)
+        WebDriver driver;
+        public WebDriver StartDriver(string browser)
         {
             switch (browser)
             {
+                case "Chrome":
+                    driver=new ChromeDriver();
+                    break;
+                case "Edge":
+                    driver=new EdgeDriver();
+                    break;
             }
+            return driver;
         }
     }
 }
